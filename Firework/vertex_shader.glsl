e2ee3ac9aeabacfac390efe83eca81817f22cf58
BrianHwang888 <bh1531@nyu.edu>
2020-04-18 14:24:52 -0400

    Fixed error where shader is unable to be found

---
+++ Firework/vertex_shader.glsl
@@ -0,0 +1,15 @@
+/*---- Vertex Shader Position & Color ----*/
+
+layout (location = 0) in vec3 vPosition;
+layout(loaction = 1) in vec4 vColor;
+ 
+out vec4 color;
+
+uniform mat4 model_view;
+uniform mat4 projection;
+void main() {
+
+	gl_Position = projection * model_view * vec4(vPosition, 1.0f);
+	color = vColor;
+
+}
