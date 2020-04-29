355d4fafde172fa50e1146d7fc19564891f22bb9
BrianHwang888 <bh1531@nyu.edu>
2020-04-29 14:04:29 -0400

    initial camera class

---
+++ Firework/camera.cpp
@@ -0,0 +1,15 @@
+#include "camera.h"
+
+
+
+camera::camera()
+{
+	camera_position = glm::vec3(0.0f, 0.0f, 3.0f);
+	camera_target = glm::vec3(0.0f, 0.0f, 0.0f);
+	camera_target_direction = glm::normalize(camera_position - camera_target);
+	up = glm::vec3(0.0f, 1.0f, 0.0f);
+	camera_right = glm::normalize(glm::cross(up, camera_target_direction));
+	camera_up = glm::normalize(glm::cross(camera_target, camera_right));
+
+	view = glm::lookAt(camera_position, camera_target, up);
+}
