63d578864fe3a19fe9d67cd0156bf6e1f4cda11c
BrianHwang888 <bh1531@nyu.edu>
2020-04-30 14:13:59 -0400

    moved camera files to respective folders (headers/source)

---
+++ Headers/camera.h
@@ -0,0 +1,28 @@
+#ifndef __CAMERA_H__
+#define __CAMERA_H__
+
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+
+/*------ Camera class ------*/
+//All directions are relative to the camera
+class camera
+{
+public:
+	glm::mat4 view;
+	camera();
+
+private:
+	glm::vec3 camera_position;
+	glm::vec3 camera_target;
+	glm::vec3 camera_target_direction;
+	glm::vec3 camera_right;
+	glm::vec3 camera_up;
+
+	glm::vec3 up;
+};
+
+#endif
